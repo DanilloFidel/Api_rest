1e8e81a714f927fb2fcbffa79219a59326d7224b
Markus Nathan Marques <markus_nathan@live.com>
2018-11-05 19:35:24 -0200

    fixbug

--- api/models/imovel.js
+++ api/models/imovel.js
@@ -15,6 +15,7 @@ module.exports = mongoose.model('Imovel', new Schema({
     cep: { type: String, required: true },
     bairro: { type: String, require: true },
     nro: { type: String, required: true },
-    imvImg: { type: String, required: true }
-
+    imvImg: [
+        { type: String, required: false }
+    ]
 }))
